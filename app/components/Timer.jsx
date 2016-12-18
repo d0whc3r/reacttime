fc90045b6dbb1823da7f0d0aa8b5d57eb0da9ac3
user <user@user>
2016-12-18 19:01:08 +0100

    add clock

--- app/components/Timer.jsx
+++ app/components/Timer.jsx
@@ -1,10 +1,12 @@
 var React = require('react');
+var Clock = require('Clock');
 
 var Timer = React.createClass({
     render: function () {
         return (
             <div>
                 <p>Timer.jsx Rendered</p>
+                <Clock totalSeconds={987}/>
             </div>
         );
     }
--- karma.conf.js
+++ karma.conf.js
@@ -2,14 +2,29 @@ var webpackConfig = require('./webpack.config');
 
 module.exports = function (config) {
     config.set({
+        // Start these browsers, currently available:
+        // - Chrome
+        // - ChromeCanary
+        // - Firefox
+        // - Opera
+        // - Safari (only Mac)
+        // - PhantomJS
+        // - IE (only Windows)
         browsers: ['Chrome'],
+        // If browser does not capture in given timeout [ms], kill it
+        captureTimeout: 60000,
+        basePath: __dirname,
         singleRun: true,
         frameworks: ['mocha'],
-        files: ['app/tests/**/*.test.jsx'],
+        files: [
+            'app/tests/**/*.test.jsx'
+        ],
         preprocessors: {
             'app/tests/**/*.test.jsx': ['webpack', 'sourcemap']
         },
         reporters: ['mocha'],
+        colors: true,
+        loglevel: config.LOG_DEBUG,
         client: {
             mocha: {
                 timeout: '5000'
@@ -17,7 +32,7 @@ module.exports = function (config) {
         },
         webpack: webpackConfig,
         webpackServer: {
-            noInfo: true
+            noInfo: false
         }
     });
 };
